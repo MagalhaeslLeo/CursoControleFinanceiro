92e523ae792a079a7ea27a7824cdd2c4ac5f30c4
MagalhaeslLeo <111078752+MagalhaeslLeo@users.noreply.github.com>
2024-10-07 13:20:00 -0300

    Criando configuração de senha do usuário no identity

--- ControleFinanceiro.API/ControleFinanceiro.API/Startup.cs
+++ ControleFinanceiro.API/ControleFinanceiro.API/Startup.cs
@@ -1,3 +1,4 @@
+using ControleFinanceiro.API.Extensions;
 using ControleFinanceiro.API.Validacoes;
 using ControleFinanceiro.API.ViewModels;
 using ControleFinanceiro.DAL;
@@ -24,96 +25,98 @@ using System.Threading.Tasks;
 
 namespace ControleFinanceiro.API
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddIdentity<Usuario, Funcao>().AddEntityFrameworkStores<Contexto>();
-            services.AddDbContext<Contexto>(opcoes => opcoes.UseSqlServer(Configuration.GetConnectionString("ConexaoBD")));
-            services.AddControllers()
-                .AddJsonOptions(opcoes =>
-            {
-                opcoes.JsonSerializerOptions.IgnoreNullValues = true;
-            })
-                               .AddNewtonsoftJson(opcoes =>
-                               {
-                                   opcoes.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-                               });
-            services.AddCors();
-
-            services.AddFluentValidationAutoValidation();
-
-            services.AddValidatorsFromAssemblyContaining<Startup>();
-
-            services.AddScoped<ICategoriaRepositorio, CategoriaRepositorio>();
-            services.AddScoped<ITipoRepositorio, TipoRepositorio>();
-            services.AddScoped<IFuncaoRepositorio, FuncaoRepositorio>();
-            services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
-
-            services.AddTransient<IValidator<Categoria>, CategoriaValidator>();
-            services.AddTransient<IValidator<FuncoesViewModel>, FuncoesViewModelValidator>();
-            services.AddTransient<IValidator<RegistroViewModel>, RegistroViewModelValidator>();
-
-            services.AddSpaStaticFiles(diretorio =>
-            {
-                diretorio.RootPath = "ControleFinanceiro-UI";
-            });
-            //services.AddSwaggerGen(c =>
-            //{
-            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApplication1", Version = "v1" });
-            //});
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                //app.UseSwagger();
-                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApplication1 v1"));
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseCors(opcoes => opcoes.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-
-            app.UseSpaStaticFiles();
-
-            app.UseStaticFiles();
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-
-            app.UseSpa(spa =>
-            {
-                spa.Options.SourcePath = Path.Combine(Directory.GetCurrentDirectory(), "ControleFinanceiro-UI");
-
-                if (env.IsDevelopment())
-                {
-                    spa.UseProxyToSpaDevelopmentServer($"http://localhost:4200/");
-                }
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddIdentity<Usuario, Funcao>().AddEntityFrameworkStores<Contexto>();
+      services.ConfigurarSenhaUsuario();
+
+      services.AddDbContext<Contexto>(opcoes => opcoes.UseSqlServer(Configuration.GetConnectionString("ConexaoBD")));
+      services.AddControllers()
+          .AddJsonOptions(opcoes =>
+      {
+        opcoes.JsonSerializerOptions.IgnoreNullValues = true;
+      })
+                         .AddNewtonsoftJson(opcoes =>
+                         {
+                           opcoes.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                         });
+      services.AddCors();
+
+      services.AddFluentValidationAutoValidation();
+
+      services.AddValidatorsFromAssemblyContaining<Startup>();
+
+      services.AddScoped<ICategoriaRepositorio, CategoriaRepositorio>();
+      services.AddScoped<ITipoRepositorio, TipoRepositorio>();
+      services.AddScoped<IFuncaoRepositorio, FuncaoRepositorio>();
+      services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
+
+      services.AddTransient<IValidator<Categoria>, CategoriaValidator>();
+      services.AddTransient<IValidator<FuncoesViewModel>, FuncoesViewModelValidator>();
+      services.AddTransient<IValidator<RegistroViewModel>, RegistroViewModelValidator>();
+
+      services.AddSpaStaticFiles(diretorio =>
+      {
+        diretorio.RootPath = "ControleFinanceiro-UI";
+      });
+      //services.AddSwaggerGen(c =>
+      //{
+      //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApplication1", Version = "v1" });
+      //});
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        //app.UseSwagger();
+        //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApplication1 v1"));
+      }
+      else
+      {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+      }
+
+      app.UseCors(opcoes => opcoes.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+      app.UseSpaStaticFiles();
+
+      app.UseStaticFiles();
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+
+      app.UseSpa(spa =>
+      {
+        spa.Options.SourcePath = Path.Combine(Directory.GetCurrentDirectory(), "ControleFinanceiro-UI");
+
+        if (env.IsDevelopment())
+        {
+          spa.UseProxyToSpaDevelopmentServer($"http://localhost:4200/");
+        }
+      });
+    }
+  }
 }
